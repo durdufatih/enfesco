d7ecd37d9de7f2026fcc2508d6d9199a51770f76
Mehmet Fatih Durdu <fatih.durdu@milliyetemlak.com>
2017-08-02 19:33:26 +0300

    Mongo için güvenlik eklendi

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const MongoClient = require('mongodb').MongoClient
 var ObjectId = require('mongodb').ObjectID;
 youTube.setKey('AIzaSyBNq9B6345OOESbftbydnyV17QdX3HrSAk');
 
-MongoClient.connect('mongodb://139.59.154.200:27017/enfesco', (err, database) => {
+MongoClient.connect('mongodb://enfesco:mfd041990!@139.59.154.200:27017/admin', (err, database) => {
    if (err) return console.log(err)
 	  db = database
     db.collection('enfesco').createIndex({"searchText": "text"})
